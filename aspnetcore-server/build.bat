f2f40960d1cbe20d452bf6fe32e14736a3cb31ca
Richard Jenkins <rcjenkins@hotmail.com>
2020-07-10 10:40:06 +0100

    fix folders

--- aspnetcore-server/build.bat
+++ aspnetcore-server/build.bat
@@ -3,7 +3,7 @@
 
 @echo off
 
-dotnet restore src\IO.Swagger
-dotnet build src\IO.Swagger
-echo Now, run the following to start the project: dotnet run -p src\IO.Swagger\IO.Swagger.csproj --launch-profile web.
+dotnet restore src\ReedExhibitionsRegistrations
+dotnet build src\ReedExhibitionsRegistrations
+echo Now, run the following to start the project: dotnet run -p src\ReedExhibitionsRegistrations\ReedExhibitionsRegistrations.csproj --launch-profile web.
 echo.
